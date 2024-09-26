1eb4c1d4117e7970af4ce43eadba4fc1bb3bd463
trigo941005 <68458908+trigo941005@users.noreply.github.com>
2024-09-27 04:06:31 +0800

    20240927-2
    line部分完成 整段程式碼完成

--- query_find.py
+++ query_find.py
@@ -1,12 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
-import time
 from selenium.common.exceptions import TimeoutException
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
 
+acToken = 'zzZxhNQcgLFKgGJnSTLxebqKWWlpTxMdCMhafgUZjk1t2EtbE9vNHU578lhbtPPTqcb4eSAmwtd1SOKd1pOXdzyK8zBiiWM6hx+9S5aZy1kDjjfaJ2Fabq20ripkSGLTd1Qi7KEhE0RDbAZweX+fnwdB04t89/1O/w1cDnyilFU='
+userID = 'U41539f029351102369df316425003bbc'
+def line_message(message):
+    line_bot_api = LineBotApi(acToken)
+    line_bot_api.push_message(userID, TextSendMessage(text=message))
 def search(user):
     driver = webdriver.Chrome()
     # 開啟目標網站
@@ -29,22 +33,23 @@ def search(user):
             EC.element_to_be_clickable((By.XPATH, "//a[text()='舊版表格格式']"))
         )
         link.click()
-        element = WebDriverWait(driver, 10).until(
+        element1 = WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.XPATH, "//td[@data-title='登記名稱']"))
         )
         # 打印內容
-        print(element.text)
-        element = WebDriverWait(driver, 10).until(
+        element2 = WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.XPATH, "//td[@data-title='登記現況']"))
         )
-        print(element.text)
+        message ="%s %s"%(element1.text,element2.text)
+        print(message)
         driver.quit
+        return message
     except TimeoutException:
         # 捕捉到 TimeoutException 時執行的代碼
-        print("查無此公司")
-#user = input("請輸入統一編號:")
-user = "1231211"
+        return "查無此公司"
+user = input("請輸入統一編號:")
 if len(user)!=8:
     print("請輸入八位數")
 else:
-    search(user)
+    line_message(search(user))
+    #search(user)
